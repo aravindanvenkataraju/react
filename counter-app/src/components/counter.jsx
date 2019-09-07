9499e69e0082bac132f2ef1960fc4bff21352bac
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-09-07 16:44:46 +1000

    get badge class dynamically

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -6,16 +6,21 @@ class Counter extends Component {
     imageUrl: "https://picsum.photos/200"
   };
   styles = {
-    fontSize: 10,
-    fontWeight: "italics"
+    fontSize: 10
   };
   render() {
     return (
       <React.Fragment>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let countBadgeClasses = "badge m-2 badge-";
+    countBadgeClasses += this.state.count === 0 ? "warning" : "primary";
+    return countBadgeClasses;
   }
 
   formatCount() {
