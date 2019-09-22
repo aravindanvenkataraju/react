43405910a60088dd440efc8c6711b376a3ca7aba
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-09-22 17:01:59 +1000

    add redirect option fo notfound pages

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -6,7 +6,7 @@ import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
 import NotFound from "./components/notFound";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
@@ -27,7 +27,10 @@ class App extends Component {
             />
             <Route path="/posts/:year?/:month?" component={Posts} />
             <Route path="/admin" component={Dashboard} />
-            <Route path="/" component={Home} />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect from="/messages" to="/posts" />
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </div>
