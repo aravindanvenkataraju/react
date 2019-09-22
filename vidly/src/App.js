11863d9a97b3e45ebbed844bac53024e5602470b
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-09-22 20:38:35 +1000

    add login form and use ref to get username field react reference

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -7,13 +7,15 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/common/navBar";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <main className="container-fluid">
+      <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm} />
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
